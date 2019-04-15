ffb589f99d0e663600fcf3f6194d37e47c0f97c3
Simon Harvey <harveysimon@gmail.com>
2019-04-15 00:53:23 -0400

    cleanup

--- Assets/Bindings/Editor/Bindings.cs
+++ Assets/Bindings/Editor/Bindings.cs
@@ -21,9 +21,6 @@ public class Bindings
 		using (var assDef = AssemblyDefinition.ReadAssembly($"Library/ScriptAssemblies/{ass.name}.dll"))
 		{
 			var modDef = assDef.Modules[0];
-			//using (var modDef = ModuleDefinition.ReadModule($"Library/ScriptAssemblies/{ass.name}.dll"))
-			//{
-			Debug.Log(modDef);
 			foreach (var t in modDef.Types)
 			{
 				var tt = t;
@@ -32,27 +29,14 @@ public class Bindings
 				if (bindableFields.Count() > 0 && t.BaseType.FullName == "System.Object")
 				{
 					t.BaseType = baseBindable;
-					/*t.BaseType = new TypeDefinition("", "BindableBase", TypeAttributes.Public | TypeAttributes.BeforeFieldInit).Resolve();
-					tt = t.Resolve();
-					Debug.Log(tt.BaseType);*/
 				}
-
-				//var changedFnDef = t.Resolve().Methods.Single(m => m.Name == "_NotifyChange").Resolve();
-				//Debug.Log(changedFnDef);
 
 				foreach (var prop in bindableFields)
 				{
-					//Debug.Log(prop);
 					prop.CustomAttributes.Remove(prop.CustomAttributes.Single(a => a.AttributeType.Name == nameof(BindableAttribute)));
 
 					var setterName = $"set_{prop.Name}";
 					var setter = t.Methods.Single(m => m.Name == setterName);
-					//Debug.Log(setter);
-
-					//var eventDef = new EventDefinition($"On{prop.Name}Changed", EventAttributes.None, new TypeDefinition("System", "Action", TypeAttributes.Import));
-					//eventDef.Resolve();
-					//t.Events.Add(eventDef);
-
 					var il = setter.Body.GetILProcessor();
 					var rr = bindableModule.ImportReference(typeof(BindableBase).GetMethod("_NotifyChange", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance));//.Resolve();
 					
@@ -64,35 +48,14 @@ public class Bindings
 					il.Append(ldstr);
 					il.Append(call);
 					il.Append(il.Create(OpCodes.Ret));
-
-					//il.InsertBefore(setter.Body.Instructions[setter.Body.Instructions.Count()-1], ldstr);
-					//il.InsertAfter(ldstr, call);
-
-					//var testNotify = baseBindable.Resolve().Methods.Single(m => m.Name == "_TestNotifyChange");
-					//var testNotify = bindableModule.ImportReference.Methods.Single(m => m.Name == "_TestNotifyChange");
-
-					//var instruction = proc.Create(OpCodes.Call, testNotify);
-					//proc.InsertAfter(setter.Body.Instructions[setter.Body.Instructions.Count - 1], instruction);
 				}
 			}
-			//}
 
 			var assName = $"{assDef.Name.Name}_Patched";
 			Debug.Log(assName);
 			assDef.Name.Name = assName;
-			//assDef.Name = new AssemblyNameDefinition(assName, assDef.Name.Version);
 			assDef.Write($"Library/ScriptAssemblies/{assName}.dll");
 		}
-	}
-
-	/*private EventDefinition GetOrCreateEventDefinition(TypeDefinition owner, string name, TypeDefinition fieldType)
-	{
-
-	}*/
-
-	public static void PatchAssemble(string srcPath, string dstPath)
-	{
-
 	}
 
 	[MenuItem("Assets/Patch Assembly", true)]
